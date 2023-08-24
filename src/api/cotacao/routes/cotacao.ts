2630d5e4510dc3f97edb056e81a1913deafba07b
Otávio Augusto <otavio.rar@gmail.com>
2023-08-24 13:48:45 -0300

    minor changes

---
+++ src/api/cotacao/routes/cotacao.ts
@@ -0,0 +1,7 @@
+/**
+ * cotacao router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cotacao.cotacao');
---
+++ src/api/cotacao/services/cotacao.ts
@@ -0,0 +1,7 @@
+/**
+ * cotacao service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::cotacao.cotacao');
